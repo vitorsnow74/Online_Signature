682c5c16bf786b2afba02a0fc5ae28cebfa0b491
Joao Vitor Santos <vitor@planner.intra>
2022-06-09 15:53:42 -0300

    fixing some bugs and adding text signature

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
 window.addEventListener("load", sign("Canvas"));    
 
-function sign(signMethod){    
-    if(signMethod == "Canvas"){
-        const canvas = document.querySelector("#canvas");
-        useCanvas(canvas);
-    }
-    if (signMethod == "Text"){
-        const text = document.querySelector();
+function sign(signMethod) {    
+    if (signMethod == "Canvas") {        
+        useCanvas();
+    } else {        
         useText();
     }    
 }
 
-function useCanvas(canvas){
-    let painting = false;         
-    const context = canvas.getContext("2d");        
-    
+function useCanvas() {
+    let painting = false;        
+    const canvas = document.querySelector("#canvas");         
+    const context = canvas.getContext("2d"); 
+
     canvas.height = 200;
-    canvas.width = 550;
+    canvas.width = window.innerWidth;               
     context.lineCap = "round";
                            
-    canvas.addEventListener("mousedown", (e) => {
+    canvas.addEventListener("mousedown", () => {
         painting = true;
-        draw(e);
+        context.lineWidth = document.querySelector("#fontWeight").value;
+        context.strokeStyle = document.querySelector(".selectedColor").value == "Preto" ? "#101010" : "#000080";              
     });
-    canvas.addEventListener("mouseup", () => {
-        painting = false;
-        context.beginPath();
-    });
-    canvas.addEventListener("mousemove", (e)=> {
-        if(!painting) return;
-        context.strokeStyle = document.querySelector(".selectedColor").value == "Azul" ? "#000080" : "#101010";
-        context.lineWidth = document.querySelector("#fontWeight").value;;
+
+    canvas.addEventListener("mousemove", (e) => {        
+        if(!painting) return; 
         context.lineTo(e.clientX, e.clientY);
         context.stroke();
         context.beginPath();
         context.moveTo(e.clientX, e.clientY);
+    });   
+     
+    window.addEventListener("mouseup", () => {
+        painting = false;
+        context.beginPath();
     });
-
+    
     document.getElementById("clearButton").onclick = () => {
         context.clearRect(0, 0, canvas.width, canvas.height);
     };
 }
 
 function useText(){
-
+    /* In creation */
+    const text = document.querySelector().value;
 }
