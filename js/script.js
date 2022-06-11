021081868c84467a5f2d7973ad95e2b66d0a73d4
vitorsnow74 <joaovitorsoares752@gmail.com>
2022-06-11 16:23:58 -0300

    now  the code it is more readble

--- js/script.js
+++ js/script.js
@@ -1,54 +1,81 @@
 window.addEventListener("load", () => {    
-    let painting = false;        
-    const text = document.querySelector("#text");
-    const canvas = document.querySelector("#canvas");    
-    const context = canvas.getContext("2d");           
-    canvas.height = 200;
-    canvas.width = window.innerWidth;          
-    context.lineCap = "round";        
+    //Page conditions
+    let painting = false;  
+    let enableDrawing = true; 
     
-                           
-    canvas.addEventListener("mousedown", () => {
+    //HTML items
+    const canvas = document.querySelector("#canvas"); 
+            canvas.height = 200;
+            canvas.width = window.innerWidth;  
+    const context = canvas.getContext("2d"); 
+            context.lineCap = "round"; 
+    const text = document.querySelector("#text");       
+    const signModeCheckbox = document.querySelector("#signModeCheckbox");
+    const signModeText = document.querySelector(".signModeText");
+    const warningMessage = document.querySelector("#warningMessage");
+    const fontFamilySelector = document.querySelector("#fontFamilySelector");                             
+       
+    //Start the page with drawing mode by default
+    selectSignMode();
+
+    //Rules
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mousemove", (e) => { keepDrawing(e) });  
+    
+    window.addEventListener("mouseup", stopDrawing);
+    window.addEventListener("resize", resize);
+
+    signModeCheckbox.addEventListener("change", selectSignMode); 
+
+    document.getElementById("clearButton").onclick = () => { clearSignature(); };            
+
+    //Functions
+    function startDrawing(){
+        if (!enableDrawing) return;
         painting = true;
         context.lineWidth = document.querySelector("#fontWeight").value;
-        context.strokeStyle = document.querySelector(".selectedColor").value == "Preto" ? "#101010" : "#000080";              
-    });
-    canvas.addEventListener("mousemove", (e) => {        
+        context.strokeStyle = document.querySelector(".selectedColor").value == "Preto" ? "#101010" : "#000080";          
+    };        
+
+    function keepDrawing(e){
         if(!painting) return; 
         context.lineTo(e.clientX, e.clientY);
         context.stroke();
         context.beginPath();
         context.moveTo(e.clientX, e.clientY);
-    });  
-    
-    window.addEventListener("mouseup", () => {
+    }
+
+    function stopDrawing(){
         painting = false;
         context.beginPath();
-    });
-    window.addEventListener("resize", () => {
+    }
+
+    function resize(){
         canvas.height = 200;
         canvas.width = window.innerWidth;            
-        context.lineCap = "round";                 
-    });
+        context.lineCap = "round"; 
+    }
 
-    document.getElementById("clearButton").onclick = () => {
+    function clearSignature(){
         context.clearRect(0, 0, canvas.width, canvas.height);
-    };
-    
-    /* In creation */    
-    const signModeCheckbox = document.querySelector("#signModeCheckbox");
-    const signModeText = document.querySelector(".signModeText");
-    signModeCheckbox.addEventListener("change", () => {        
-        if(signModeCheckbox.checked) {
-            // Checkbox está selecionado.            
+    }
+
+    function selectSignMode(){
+        if(signModeCheckbox.checked) { 
+            enableDrawing = true;                     
             text.style.display = "none";              
             signModeText.innerText = "Desenhar";
-            signModeText.style.color = "blue";            
-        } else {
-            // Checkbox não está selecionado.
+            signModeText.style.color = "blue";
+            warningMessage.style.display = "none"; 
+            fontFamilySelector.style.display = "none";           
+        } else {     
+            clearSignature();       
+            enableDrawing = false;
             text.style.display = "initial"; 
             signModeText.innerText = "Digitar"; 
             signModeText.style.color = "red";
-        }              
-    });    
+            warningMessage.style.display = "block";   
+            fontFamilySelector.style.display = "block"; 
+        }    
+    }                          
 });
